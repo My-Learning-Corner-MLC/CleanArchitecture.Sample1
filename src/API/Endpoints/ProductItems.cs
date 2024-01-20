f457f2739be635ad60195bf6fd605c408927edda
Vi Diec <diecluchivi2701@gmail.com>
2024-01-20 22:08:21 +0700

    add: unit test setup

--- src/API/Endpoints/ProductItems.cs
+++ src/API/Endpoints/ProductItems.cs
@@ -1,6 +1,8 @@
 using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Sample1.API.Infrastructure;
 using Sample1.Application.Common.Exceptions;
+using Sample1.Application.Common.Models;
 using Sample1.Application.ProductItems.Queries.GetProductItemDetail;
 using Sample1.Application.ProductItems.Queries.GetProductItemsWithPagination;
 
@@ -15,17 +17,17 @@ public class ProductItems : EndpointGroupBase
             .MapPost(GetProductItemsWithPagination);
     }
 
-    public async Task<IResult> GetProductItemDetail(ISender sender, int id)
+    public async Task<Results<Ok<ProductItemDetailDto>, NotFound, BadRequest>> GetProductItemDetail(ISender sender, int id)
     {
         if (id < 0) throw new ValidationException("Id at least greater than or equal to 0.");
 
         var item = await sender.Send(new GetProductItemDetailQuery { Id = id });
 
-        return Results.Ok(item);
+        return TypedResults.Ok(item);
     }
 
-    public async Task<IResult> GetProductItemsWithPagination(ISender sender, GetProductItemsWithPaginationQuery query)
+    public async Task<Results<Ok<PaginatedList<ProductItemBriefDto>>, BadRequest>> GetProductItemsWithPagination(ISender sender, GetProductItemsWithPaginationQuery query)
     {
-        return Results.Ok(await sender.Send(query));
+        return TypedResults.Ok(await sender.Send(query));
     }
 }
---
+++ test/Sample1.UnitTests/Utils/DataMockHelper.cs
@@ -0,0 +1,36 @@
+using Bogus;
+using Sample1.Application.ProductItems.Queries.GetProductItemDetail;
+using Sample1.Domain.Entities;
+
+namespace Sample1.UnitTests.Utils;
+
+public static class DataMockHelper
+{
+    public static ProductItem GetProductItemMock(int? id = default)
+    {
+        var productFaker = new Faker<ProductItem>()
+            .RuleFor(o => o.Id, f => id ?? f.UniqueIndex)
+            .RuleFor(o => o.Name, f => f.Name.FullName())
+            .RuleFor(o => o.Description, f => f.Lorem.Paragraph())
+            .RuleFor(o => o.Price, f => f.Random.Decimal(1, 100))
+            .RuleFor(o => o.PictureFileName, f => f.Lorem.Text())
+            .RuleFor(o => o.PictureUri, f => f.Internet.Url())
+            .RuleFor(o => o.ProductTypeId, f => f.Random.Int(1, 100))
+            .RuleFor(o => o.ProductBrandId, f => f.Random.Int(1, 100));
+
+        return productFaker;
+    }
+
+    public static ProductItemDetailDto GetProductItemDetailDtoMock(int? id = default)
+    {
+        var productDetailDtoFaker = new Faker<ProductItemDetailDto>()
+            .RuleFor(o => o.Id, f => id ?? f.UniqueIndex)
+            .RuleFor(o => o.Name, f => f.Name.FullName())
+            .RuleFor(o => o.Description, f => f.Lorem.Paragraph())
+            .RuleFor(o => o.Price, f => f.Random.Decimal(1, 100))
+            .RuleFor(o => o.PictureFileName, f => f.Lorem.Text())
+            .RuleFor(o => o.PictureUri, f => f.Internet.Url());
+
+        return productDetailDtoFaker;
+    }
+}
